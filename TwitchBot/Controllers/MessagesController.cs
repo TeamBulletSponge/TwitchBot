c98351a1e6ba26d5dd8e7091d033d6300cfc54f5
Brian Ingenito <brian_ingenito@hotmail.com>
2016-04-02 15:06:47 -0400

    Add /status command to display online/offline status. If online, include preview image of stream.

--- TwitchBot/Controllers/MessagesController.cs
+++ TwitchBot/Controllers/MessagesController.cs
@@ -1,19 +1,20 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Connector.Utilities;
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System;
 
 namespace TwitchBot
 {
   [BotAuthentication]
   public class MessagesController : ApiController
   {
+    private static readonly string KrakenUrl = "https://api.twitch.tv/kraken/";
+    private static readonly WebClient WebClient = new WebClient();
+
     /// <summary>
     /// POST: api/Messages
     /// Receive a message from a user and reply to it
@@ -22,11 +23,42 @@ namespace TwitchBot
     {
       if (message.Type == "Message")
       {
-        // calculate something for us to return
-        int length = (message.Text ?? string.Empty).Length;
+        string[] tokens = message.Text.Split(' ');
+        if (tokens == null || tokens.Length == 0)
+        {
+          return null;
+        }
 
-        // return our reply to the user
-        return message.CreateReplyMessage($"You sent {length} characters");
+        if (tokens[0] == "/status")
+        {
+          if (tokens.Length == 1)
+          {
+            return message.CreateReplyMessage("You must provide a channel name: /status {channel}");
+          }
+
+          string channel = tokens[1];
+          string url = KrakenUrl + "streams/" + channel;
+          var json = WebClient.DownloadString(url);
+          JObject result = JObject.Parse(json);
+
+          if (result["stream"].HasValues)
+          {
+            Attachment previewAttachment = new Attachment();
+            previewAttachment.ContentUrl = result["stream"]["preview"]["small"].ToString(); ;
+            previewAttachment.ContentType = "image/png";
+            Message reply = message.CreateReplyMessage(String.Format("{0} is online since {1}.{2}", channel, result["stream"]["created_at"].ToString(), Environment.NewLine));
+            reply.Attachments = new List<Attachment>();
+            reply.Attachments.Add(previewAttachment);
+
+            return reply;
+          }
+          else
+          {
+            return message.CreateReplyMessage(channel + " is offline.");
+          }
+        }
+
+        return null;
       }
       else
       {
